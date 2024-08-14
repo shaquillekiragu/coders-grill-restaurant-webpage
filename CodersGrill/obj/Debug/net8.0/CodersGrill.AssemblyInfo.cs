731e079f5dd01cd32fa76a1067f207b8c5b36a33
Shaquille Kiragu <shaquille.kiragu@outlook.com>
2024-08-15 00:07:11 +0100

    Updating the main page footer

--- CodersGrill/obj/Debug/net8.0/CodersGrill.AssemblyInfo.cs
+++ CodersGrill/obj/Debug/net8.0/CodersGrill.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodersGrill")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db340651ceb2f47c8bbaad3b8d5dfa0b358df326")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b251460a0da4cfaff4faf710a5f4c14079707563")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodersGrill")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodersGrill")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
